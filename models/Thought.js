5ddf8bc2c17b44ca9829f38c083d0d789f1e24b0
Ozanne Alexander <alexanoz@outlook.com>
2023-03-07 20:20:26 -0500

    moved the reaction model code to thought.js due to hw instructions have to rework this model

--- models/Thought.js
+++ models/Thought.js
@@ -4,11 +4,39 @@ const reactionSchema = require('./Reaction');
 const ThoughtSchema = new Schema (
     {
         // thoughtText
-        // username
         // createdAt:
+        // username
+        // reactions
 
 
 
+
+    // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+})
+
+// REACTION GOES HERE 
+
+const ReactionSchema = new Schema({
+   
+    // objectID data type for Mongoose
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        // you'll want to set a new default value here
+        default: () =>new Types.ObjectId(),
+    },
+    // reactionBody:
+    reactionBody:{
+        type: String,
+        required: true,
+        // you can utilitze maxlength or validate here for the 280 character
+        maxLength: 280,
+    },
+    // username
+    username:{
+        type: String,
+        required: true,
+    }
+    // createdAt
     
 })
 
