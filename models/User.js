0de6ada0eb6d5e2fe2844d2fbe9e6fdb63e68cae
Ozanne Alexander <alexanoz@outlook.com>
2023-03-07 19:59:38 -0500

    finished User model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,32 @@
 // need username; email; thought array; friend array
 const {Schema, model, Types} = require('mongoose');
 
-const userSchema = new Schema ({
+const UserSchema = new Schema ({
 
     // username
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
     // email
+    email:{
+        type: String,
+        unique: true,
+        required: true,
+        // mongoose matching should go here /.+\@.+\..+/
+        match: /.+\@.+\..+/,
+    },
+    thoughts: [],
+    friends:[]
 
-    // we need JSON
+});
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
 })
+
+const User = model('User', UserSchema);
 
 module.exports = User;
