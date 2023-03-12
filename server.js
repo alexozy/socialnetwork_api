e7e9b972030f75c90af9cfc63086e645695ff630
Ozanne Alexander <alexanoz@outlook.com>
2023-03-12 11:38:13 -0400

    added server.js to commit

--- server.js
+++ server.js
@@ -10,8 +10,24 @@ const PORT = process.env.PORT || 3001;
 // express middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+// MONGO
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/socialnetwork_api",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+// executed mongo queries
+    mongoose.set('debug', true);
+// this code isn't working
+// db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server for ${activity} running on port ${PORT}!`);
+//     });
 
 app.listen(PORT, () => console.log (`Listening on ${PORT}`));
