c3a6b2490798491c3ed6d623b77b9ef160cba34b
Ozanne Alexander <alexanoz@outlook.com>
2023-03-12 21:18:51 -0400

    added create new thought

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -12,11 +12,30 @@ const thoughtController = {
         })
     },
     // One thought
-    getThoughtID(req, res) {
-        Thought.findOne({_id:URLSearchParams.thoughtID})
-        .then ((dbData) => {
-            // if statement for dbData
-            if(!dbData){ res.status(404).json}
-        });
-    }
+    getThoughtID({ params }, res) {
+        Thought.findOne({ _id: params.thoughtID })
+            .then((dbData) => {
+                // if statement for dbData
+                if (!dbData) { res.status(404).json }
+                res.json(dbData);
+            });
+
+    },
+},
+createThought({ body, params}, res){
+    Thought.create(body)
+    .then(({_id}) => {
+        return User.findOneAndUpdate(
+            // id
+            {_id: params.userID},
+            // push it
+            {$push: {thoughts: _id} 
+        },
+            //  new thought returned
+            {new: true}
+        );
+    })
+    .then(dbData) => res.json(dbData)
+    .catch((err)+> rest.status(400).json(err))
 }
+
