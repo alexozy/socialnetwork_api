01e37549d4f014fc60f56fbd31a405bb4e43c997
Ozanne Alexander <alexanoz@outlook.com>
2023-03-12 21:45:09 -0400

    corrected require spelling in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 // import your apis for the index.js routes in the api folder not the index.js
-const apiRoutes = reqiure ('./api');
+const apiRoutes = require ('./api');
 
 router.use('./api', apiRoutes);
 router.use((req, res) => {
