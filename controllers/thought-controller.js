ab64f8366c9bd6580977bd49186edac507e81cbd
Ozanne Alexander <alexanoz@outlook.com>
2023-03-13 23:13:29 -0400

    moved the thought controller into the right file

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,12 +1,30 @@
 const {Thought, User} = require("../models");
 
 const thoughtController = {
-    // GET ALL
-    getAllThoughts(req, res){
+
+    // GET ALL thoughts
+    getAllThought(req,res) {
         Thought.find({})
-        .then((dbData) => res.json(dbData))
-        .catch(err=>{
-            console.log(err)
+        .then((dbData) => res.json (dbData))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
         })
-    }
+    },
+    // GET One thought
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtID })
+            .then((dbData) => {
+                // if statement for dbData when no thought ID is found
+                if (!dbData) { 
+                    res.status(404).json ({ message: "No thought assigned to this ID"});
+                    return;
+                }
+                res.json(dbData);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
+    },
 }
